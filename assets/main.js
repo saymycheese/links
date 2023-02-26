b3f8ef08546852e792ecaac638e9e4a98d881b55
saymycheese <chi.pham@newschool.edu>
2023-02-26 14:38:08 -0500

    updates

--- assets/main.js
+++ assets/main.js
@@ -2,27 +2,75 @@ window.arenaCallback = () => {
 	// Put your JavaScript in here!
 	// This will run after Are.na’s API returns your data.
 
+    //image lightbox
     let imageBlocks = document.querySelectorAll('li.image-block')
 
     imageBlocks.forEach((block) => {
         block.querySelector('figure').onclick = () => {
+            block.classList.add("active")
+
+            block.querySelector('aside').onclick = () => {
+                console.log("click")
+                block.classList.remove("active")
+            }
+        }
+    })
+
+
+    //video embed lightbox
+    let videoEmbedBlocks = document.querySelectorAll('li.video-embed-block')
+
+    videoEmbedBlocks.forEach((block) => {
+        block.querySelector('figure').onclick = () => {
             block.classList.add('active')
 
             block.querySelector('aside').onclick = () => {
+                console.log("click")
                 block.classList.remove('active')
             }
         }
     })
 
-    let videoEmbedBlocks = document.querySelectorAll('li.video-embed-block')
+    //link lightbox
+    let linkBlocks = document.querySelectorAll('li.link-block')
 
-    videoEmbedBlocks.forEach((block) => {
+    linkBlocks.forEach((block) => {
         block.querySelector('figure').onclick = () => {
             block.classList.add("active")
 
             block.querySelector('aside').onclick = () => {
+                console.log("click")
                 block.classList.remove("active")
             }
         }
     })
+    
+
+
+
+    //play and pause button
+    
+    var track = document.getElementById('track');
+
+    var controlBtn = document.getElementById('play-pause');
+    
+    function playPause() {
+        if (track.paused) {
+            track.play();
+            //controlBtn.textContent = "Pause";
+            controlBtn.className = "pause";
+        } else { 
+            track.pause();
+             //controlBtn.textContent = "Play";
+            controlBtn.className = "play";
+        }
+    }
+    
+    controlBtn.addEventListener("click", playPause);
+    track.addEventListener("ended", function() {
+      controlBtn.className = "play";
+    });
+
+    //
+
 }
